c3292619db5ab1ccb384235870d976200fb13aff
xshell <cste025@gmail.com>
2017-10-15 16:46:05 +0200

    date bugs

--- lib/obd_conn.py
+++ lib/obd_conn.py
@@ -31,8 +31,8 @@ class OBDConn:
             cmd_type = "{}{}".format(hex(event.command.mode), hex(event.command.pid)).replace("0x", '')
             if event.value:
                 try:
-                    value = event.value.magnitude
-                    unit = event.value.u
+                    value = event.value.u
+                    unit = event.value.magnitude
                 except AttributeError:
                     value = event.value
                     unit = "unknown"
